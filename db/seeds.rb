a672be1e12c3c55dc7e93bb478a6571ec3425823
szyar <soezayarszyar@gmail.com>
2020-09-09 21:47:21 +0630

    create users via seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Eve", password: "helloeve")
+User.create(username: "Adam", password: "helloadam")
+User.create(username: "Bruce", password: "hellobruce")
+User.create(username: "Selena", password: "helloselena")
+User.create(username: "Talia", password: "hellotalia")
